29d73ace24b030b1fb3ebd5131f07eb687538511
Wei Cheng <michael1996623@gmail.com>
2022-01-12 23:08:19 +0800

    feat-Add clock gating cells in conv module (not enabled)
    - Add manual clocking gating for conv 3x3 1x1 max pooling module

--- clipQ_hw/src/conv.sv
+++ clipQ_hw/src/conv.sv
@@ -28,6 +28,7 @@ module conv
   sp_ram_intf input_conv_3x3_intf ();
   sp_ram_intf output_conv_3x3_intf ();
   logic finish_conv_3x3, start_conv_3x3;
+  logic gated_conv_3x3_clk;
 
   // For 1x1 conv unit
   sp_ram_intf param_conv_1x1_intf ();
@@ -36,6 +37,7 @@ module conv
   sp_ram_intf input_conv_1x1_intf ();
   sp_ram_intf output_conv_1x1_intf ();
   logic finish_conv_1x1, start_conv_1x1;
+  logic gated_conv_1x1_clk;
 
   // For max pooling unit  
   sp_ram_intf param_maxpool_intf ();
@@ -44,28 +46,33 @@ module conv
   sp_ram_intf input_maxpool_intf ();
   sp_ram_intf output_maxpool_intf ();
   logic finish_maxpool, start_maxpool;
+  logic gated_maxpool_clk;
 
   bus_switcher i_bus_switcher (
       .mode(mode),
       // External
+      .clk(clk),
       .param_o(param_intf),
       .bias_o(bias_intf),
       .weight_o(weight_intf),
       .input_o(input_intf),
       .output_o(output_intf),
       // For 3x3 conv unit
+      .gated_conv_3x3_clk(gated_conv_3x3_clk),
       .param_conv_3x3_i(param_conv_3x3_intf),
       .bias_conv_3x3_i(bias_conv_3x3_intf),
       .weight_conv_3x3_i(weight_conv_3x3_intf),
       .input_conv_3x3_i(input_conv_3x3_intf),
       .output_conv_3x3_i(output_conv_3x3_intf),
       // For 1x1 conv unit
+      .gated_conv_1x1_clk(gated_conv_1x1_clk),
       .param_conv_1x1_i(param_conv_1x1_intf),
       .bias_conv_1x1_i(bias_conv_1x1_intf),
       .weight_conv_1x1_i(weight_conv_1x1_intf),
       .input_conv_1x1_i(input_conv_1x1_intf),
       .output_conv_1x1_i(output_conv_1x1_intf),
       // For max pooling unit
+      .gated_maxpool_clk(gated_maxpool_clk),
       .param_maxpool_i(param_maxpool_intf),
       .bias_maxpool_i(bias_maxpool_intf),
       .weight_maxpool_i(weight_maxpool_intf),
@@ -94,7 +101,7 @@ module conv
 
   conv_1x1 i_conv_1x1 (
       .rst(rst),
-      .clk(clk),
+      .clk(clk), // gated_conv_1x1_clk
       .w8(w8),
       .start(start_conv_1x1),
       .finish(finish_conv_1x1),
@@ -107,7 +114,7 @@ module conv
 
   conv_3x3 i_conv_3x3 (
       .rst(rst),
-      .clk(clk),
+      .clk(clk), // gated_conv_3x3_clk
       .w8(w8),
       .start(start_conv_3x3),
       .finish(finish_conv_3x3),
@@ -119,7 +126,7 @@ module conv
   );
 
   max_pool i_max_pool (
-      .clk(clk),
+      .clk(clk), // gated_maxpool_clk
       .rst(rst),
       .start(start_maxpool),
       .finish(finish_maxpool),
