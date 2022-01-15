cfb3503b7023a527dad9ca226abff0ae5eb4ab8f
Wei Cheng <michael1996623@gmail.com>
2022-01-15 14:12:30 +0800

    maint-File rename

--- VSD_2021/include/ConvAcc.svh
+++ VSD_2021/include/ConvAcc.svh
@@ -14,7 +14,9 @@
 `define READ_ENB (1'b1)
 `define READ_DIS (1'b0)
 
-`define PADDING_TYPE_WIDTH 2
-`define PARAM_WIDTH 16
+`define CONV_1x1_MODE 4'h1
+`define MAX_POOL_MODE 4'h2
+`define CONV_3x3_MODE 4'h3
+`define IDLE_MODE     4'h0
 
 `endif
