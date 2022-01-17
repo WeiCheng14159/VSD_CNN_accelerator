748cd97b27d1dc2bae991342379558cafc4ee2f6
Wei Cheng <michael1996623@gmail.com>
2022-01-17 16:34:47 +0800

    feat-Link EPU data with main program
    - Write a python script to convert hex data for EPU to binary format.
    - Use gcc objdump program to link generated binary data into main
    program.
    - Use linker script to control where data will be placed.

---
+++ VSD_2021/sim/conv0/conv2bin.py
@@ -0,0 +1,105 @@
+import sys
+
+
+class FileReader:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def open(self):
+        print("Reading file ", self.filename)
+        try:
+            self.file = open(self.filename, 'r')
+        except OSError:
+            print("Cannot open file ", self.filename)
+            sys.exit(-1)
+        return self.file
+
+    def get_filename(self):
+        return self.filename
+
+    def close(self):
+        if hasattr(self, 'file'):
+            self.file.close()
+            self.__del__()
+
+
+class FileWriter:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def open(self):
+        print("Writing to file ", self.filename)
+        try:
+            self.file = open(self.filename, 'wb')
+        except OSError:
+            print("Cannot open file ", self.filename)
+            sys.exit(-1)
+        return self.file
+
+    def get_filename(self):
+        return self.filename
+
+    def close(self):
+        if hasattr(self, 'file'):
+            self.file.close()
+            self.__del__()
+
+if __name__ == "__main__":
+
+    BYTE_ZERO = "00"
+
+    # Convert In8.hex
+    ifname = "./In8.hex"
+    ifname_prefix = ifname.split(".hex")[0]
+    ofname = ifname_prefix+".dat"
+    rf = FileReader(ifname).open()
+    wf = FileWriter(ofname).open()
+    lines = rf.readlines()
+    for l in lines:
+        line = l.rstrip("\n")
+        wb = bytes.fromhex(BYTE_ZERO + BYTE_ZERO + BYTE_ZERO + line)
+        wf.write(wb)
+    rf.close()
+    wf.close()
+
+    # Convert param.hex
+    ifname = "./param.hex"
+    ifname_prefix = ifname.split(".hex")[0]
+    ofname = ifname_prefix+".dat"
+    rf = FileReader(ifname).open()
+    wf = FileWriter(ofname).open()
+    lines = rf.readlines()
+    for l in lines:
+        line = l.rstrip("\n")
+        wb = bytes.fromhex(line)
+        wf.write(wb)
+    rf.close()
+    wf.close()
+
+    # Convert W2.hex
+    ifname = "./W2.hex"
+    ifname_prefix = ifname.split(".hex")[0]
+    ofname = ifname_prefix+".dat"
+    rf = FileReader(ifname).open()
+    wf = FileWriter(ofname).open()
+    lines = rf.readlines()
+    for l in lines:
+        line = l.rstrip("\n")
+        wb = bytes.fromhex("000"+line)
+        wf.write(wb)
+    rf.close()
+    wf.close()
+
+    # Convert Bias32.hex
+    ifname = "./Bias32.hex"
+    ifname_prefix = ifname.split(".hex")[0]
+    ofname = ifname_prefix+".dat"
+    rf = FileReader(ifname).open()
+    wf = FileWriter(ofname).open()
+    lines = rf.readlines()
+    for l in lines:
+        line = l.rstrip("\n")
+        wb = bytes.fromhex(line)
+        wf.write(wb)
+    rf.close()
+    wf.close()
