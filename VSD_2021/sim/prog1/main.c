12670a4ded7241ae9024b1612c143f45a78be284
Wder4 <wang655370@gmail.com>
2022-01-17 16:59:05 +0800

    Backup
    - Add counter for DRAM in DMA

--- VSD_2021/sim/prog1/main.c
+++ VSD_2021/sim/prog1/main.c
@@ -10,12 +10,11 @@ volatile unsigned int *sensor_addr = (int *) 0x10000000;
  *****************************************************************/
 void copy () {
   int i;
-  for (i = 0; i < sensor_size; i++) { // Copy data from sensor controller to DM
-    *(copy_addr + i) = sensor_addr[i];
-  }
-  // setDMA(sensor_addr, copy_addr, sensor_size);
+  // for (i = 0; i < sensor_size; i++) { // Copy data from sensor controller to DM
+  //   *(copy_addr + i) = sensor_addr[i];
+  // }
+  setDMA(sensor_addr, copy_addr, sensor_size);
   copy_addr += sensor_size; // Update copy address
-  asm("addi t3, x0, 1");
   copy_count++;    // Increase copy count
   sensor_addr[0x80] = 1; // Enable sctrl_clear
   sensor_addr[0x80] = 0; // Disable sctrl_clear
@@ -26,24 +25,35 @@ void copy () {
   return;
 }
 
-/*****************************************************************
- * Function: void sort(int *, unsigned int)                                    *
+	/*****************************************************************
+ * Function: void Quick Sort                                     *
  * Description: Sorting data                                     *
  *****************************************************************/
-void sort(int *array, unsigned int size) {
-  int i, j;
-  int temp;
-  for (i = 0; i < size - 1; i++) {
-    for (j = i + 1; j < size; j++) {
-      if (array[i] > array[j]) {
-        temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-      }
+void swap (int* a, int* b) {
+    int temp = *a;
+    *a = *b; 
+    *b = temp;
+}
+// return partition index
+int partition(int array[], int low, int high) {
+    int pivot = array[high];
+    int i = low - 1;
+    for (int j = low; j <= high - 1; j++) {
+        if (array[j] < pivot) {
+            i++;
+            swap(&array[i], &array[j]);
+        }
     }
-  }
+    swap(&array[i + 1], &array[high]);
+    return (i+1);
+}
 
-  return;
+void QuickSort(int array[], int low, int high) {
+    if (low < high) {
+        int partition_idx = partition(array, low, high);
+        QuickSort(array, low, partition_idx - 1);
+        QuickSort(array, partition_idx + 1, high);
+    }
 }
 
 int main(void) {
@@ -53,11 +63,11 @@ int main(void) {
   copy_addr = &_test_start;
 
   // Enable Global Interrupt
-  asm("csrsi mstatus, 0x8"); // MIE of mstatus
+  // asm("csrsi mstatus, 0x8"); // MIE of mstatus
 
-  // Enable Local Interrupt
-  asm("li t6, 0x800");
-  asm("csrs mie, t6"); // MEIE of mie 
+  // // Enable Local Interrupt
+  // asm("li t6, 0x800");
+  // asm("csrs mie, t6"); // MEIE of mie 
 
   // Enable Sensor Controller
   sensor_addr[0x40] = 1; // Enable sctrl_en
@@ -70,7 +80,8 @@ int main(void) {
     }
 
     // Start sorting
-    sort(sort_addr, sensor_size);
+    // sort(sort_addr, sensor_size);
+    QuickSort(sort_addr, 0, sensor_size - 1);
     sort_addr += sensor_size;
     sort_count++;
   }
