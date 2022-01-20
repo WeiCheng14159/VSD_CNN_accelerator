445eafc4fc26d270cd71edaca97db857873100c7
Wei Cheng <michael1996623@gmail.com>
2022-01-20 16:27:24 +0800

    maint-Move commonly used scripts here

--- VSD_2021/sim/tools/conv2bin.py
+++ VSD_2021/sim/tools/conv2bin.py
@@ -78,6 +78,21 @@ if __name__ == "__main__":
     rf.close()
     wf.close()
 
+    # Convert Out8.hex
+    ifname = "./Out8.hex"
+    ifname_prefix = ifname.split(".hex")[0]
+    ofname = ifname_prefix+".dat"
+    rf = FileReader(ifname).open()
+    wf = FileWriter(ofname).open()
+    lines = rf.readlines()
+    for l in lines:
+        line = l.rstrip("\n")
+        wb = bytearray.fromhex(BYTE_ZERO + BYTE_ZERO + BYTE_ZERO + line)
+        wb.reverse()
+        wf.write(wb)
+    rf.close()
+    wf.close()
+
     # Convert W2.hex
     ifname = "./W2.hex"
     ifname_prefix = ifname.split(".hex")[0]
@@ -103,21 +118,6 @@ if __name__ == "__main__":
     for l in lines:
         line = l.rstrip("\n")
         wb = bytearray.fromhex(line)
-        wb.reverse()
-        wf.write(wb)
-    rf.close()
-    wf.close()
-
-    # Convert Out8.hex
-    ifname = "./Out8.hex"
-    ifname_prefix = ifname.split(".hex")[0]
-    ofname = ifname_prefix+".dat"
-    rf = FileReader(ifname).open()
-    wf = FileWriter(ofname).open()
-    lines = rf.readlines()
-    for l in lines:
-        line = l.rstrip("\n")
-        wb = bytearray.fromhex(BYTE_ZERO + BYTE_ZERO + BYTE_ZERO + line)
         wb.reverse()
         wf.write(wb)
     rf.close()
