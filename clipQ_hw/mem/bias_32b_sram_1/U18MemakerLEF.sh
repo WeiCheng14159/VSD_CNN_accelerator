f675ded19bc40ad9016bb6aff059e2ebabceaf81
Wei Cheng <michael1996623@gmail.com>
2022-01-21 19:42:19 +0800

    maint-Remove unused mem files

--- clipQ_hw/mem/bias_32b_sram_1/U18MemakerLEF.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sed -i 's/ME/metal/g' $1
-sed -i 's/VI1/via/g' $1
-sed -i 's/VI/via/g' $1
-sed -i 's/NAmetalSCASESENSITIVE/NAMESCASESENSITIVE/g' $1
-sed -i 's/SYMmetalTRY/SYMMETRY/g' $1
-sed -i 's/ABUTmetalNT/ABUTMENT/g' $1
