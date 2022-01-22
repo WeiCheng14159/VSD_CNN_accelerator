ec67e42b72e45a960151afa60ca1050696dbc2f3
Wder4 <wang655370@gmail.com>
2022-01-22 22:55:42 +0800

    Refine critical path

--- VSD_2021/src/Master.sv
+++ VSD_2021/src/Master.sv
@@ -16,7 +16,7 @@ module Master(
     output logic [`DATA_BITS-1:0] dataout_o,
     output logic                  wait_o
 );
-// {{{
+
     // STATE
     logic [2:0] STATE, NEXT;
     parameter IDLE  = 3'h0,
@@ -24,45 +24,20 @@ module Master(
               AW_CH = 3'h3, W_CH = 3'h4, B_CH = 3'h5;
     // Handshake
     logic awhns, arhns, rhns, whns, bhns;
-    // Other
     logic rdfin, wrfin;
-    logic req_rd, req_wr;
-    logic [`AXI_STRB_BITS-1:0] wstrb;
+    // AXI
+    logic [`AXI_STRB_BITS-1:0] wstrb_r;
+    logic [`AXI_LEN_BITS -1:0] arlen_r;
 
-// }}}
-    // Handshake
+// {{{ Handshake
     assign awhns = m2axi_i.awready & m2axi_o.awvalid;
     assign whns  = m2axi_i.wready  & m2axi_o.wvalid;
     assign bhns  = m2axi_o.bready  & m2axi_i.bvalid;
     assign arhns = m2axi_i.arready & m2axi_o.arvalid;
     assign rhns  = m2axi_o.rready  & m2axi_i.rvalid;
-    // Finish
     assign rdfin = rhns & m2axi_i.rlast;
     assign wrfin = whns & m2axi_o.wlast;
-    // Sample
-
-    always_comb begin
-        case (cwtype_i[1:0])
-            2'b00   : wstrb = `AXI_STRB_BYTE;
-            2'b01   : wstrb = `AXI_STRB_HWORD;
-            default : wstrb = `AXI_STRB_WORD;
-        endcase
-    end
-    //
-    assign m2axi_o.arid    = `AXI_ID_BITS'h0;
-    assign m2axi_o.arlen   = arlenone_i ? `AXI_LEN_ONE : `AXI_LEN_FOUR;
-    assign m2axi_o.arsize  = `AXI_SIZE_BITS'b10;
-    assign m2axi_o.arburst = `AXI_BURST_FIXED;
-    assign m2axi_o.araddr  = caddr_i;
-    assign m2axi_o.awid    = `AXI_ID_BITS'h0;
-    assign m2axi_o.awlen   = `AXI_LEN_ONE;
-    assign m2axi_o.awsize  = `AXI_SIZE_BITS'b10;
-    assign m2axi_o.awburst = `AXI_BURST_FIXED;
-    assign m2axi_o.awaddr  = caddr_i;
-    assign m2axi_o.wstrb   = wstrb;
-    assign m2axi_o.wlast   = 1'b1;
-    assign m2axi_o.wdata   = cdatain_i;
-    assign dataout_o       = m2axi_i.rdata;
+// }}}
 
 // {{{ STATE
     always_ff @(posedge clk or negedge rst) begin
@@ -72,7 +47,7 @@ module Master(
         NEXT = IDLE;
         case (STATE)
             IDLE    : begin
-                case ({m2axi_o.arvalid, m2axi_o.awvalid})
+                case ({crreq_i, cwreq_i})
                     2'b10   : NEXT = arhns ? R_CH : AR_CH;
                     2'b01   : NEXT = awhns ? W_CH : AW_CH;
                     default : NEXT = IDLE;
@@ -98,44 +73,51 @@ module Master(
 // }}}	
 
 // {{{ AXI
-    assign req_rd = crreq_i;//creq_i && ~cwrite_i;
-    assign req_wr = cwreq_i;//creq_i && cwrite_i;
+    always_ff @(posedge clk or negedge rst) begin
+        if (~rst)            arlen_r <= `AXI_LEN_BITS'h0;
+        else if (rdfin)      arlen_r <= `AXI_LEN_FOUR;
+        else if (arlenone_i) arlen_r <= `AXI_LEN_ONE;
+    end
+    
+    always_ff @(posedge clk or negedge rst) begin
+        case (cwtype_i[1:0])
+            2'b00   : wstrb_r <= `AXI_STRB_BYTE;
+            2'b01   : wstrb_r <= `AXI_STRB_HWORD;
+            default : wstrb_r <= `AXI_STRB_WORD;
+        endcase
+    end
 
-    logic [2:0] validout;
-    logic [1:0] readyout;
-    assign {m2axi_o.arvalid, m2axi_o.awvalid, m2axi_o.wvalid} = validout;
-    assign {m2axi_o.rready, m2axi_o.bready} = readyout;
+    assign m2axi_o.arid    = `AXI_ID_BITS'h0;
+    assign m2axi_o.arlen   = arlenone_i ? `AXI_LEN_ONE : `AXI_LEN_FOUR;
+    assign m2axi_o.arsize  = `AXI_SIZE_BITS'b10;
+    assign m2axi_o.arburst = `AXI_BURST_FIXED;
+    assign m2axi_o.araddr  = caddr_i;
+    assign m2axi_o.awid    = `AXI_ID_BITS'h0;
+    assign m2axi_o.awlen   = `AXI_LEN_ONE;
+    assign m2axi_o.awsize  = `AXI_SIZE_BITS'b10;
+    assign m2axi_o.awburst = `AXI_BURST_FIXED;
+    assign m2axi_o.awaddr  = caddr_i;
+    assign m2axi_o.wstrb   = wstrb_r;
+    assign m2axi_o.wlast   = 1'b1;
+    assign m2axi_o.wdata   = cdatain_i;
+    assign dataout_o       = m2axi_i.rdata;
+    // valid
+    assign m2axi_o.arvalid = STATE == AR_CH;
+    assign m2axi_o.awvalid = STATE == AW_CH;
+    assign m2axi_o.wvalid  = STATE == W_CH;
+    // ready
+    assign m2axi_o.rready = STATE == R_CH;
+    assign m2axi_o.bready = STATE == B_CH;
+
     always_comb begin
         case (STATE)
-            IDLE    : validout = {req_rd, req_wr, 1'b0};
-            AR_CH   : validout = 3'b100;
-            R_CH    : validout = 3'b0;
-            AW_CH   : validout = 3'b10;
-            W_CH    : validout = 3'b1;
-            B_CH    : validout = 3'b0;
-            default : validout = 3'b0;
-        endcase
-    end
-    always_comb begin
-        case (STATE)
-            IDLE    : readyout = 2'b0;
-            AR_CH   : readyout = 2'b0;
-            R_CH    : readyout = 2'b10;
-            AW_CH   : readyout = 2'b0;
-            W_CH    : readyout = {1'b0, wrfin};
-            B_CH    : readyout = 2'b1;
-            default : readyout = 2'b0;
-        endcase
-    end
-    always_comb begin
-        case (STATE)
-            IDLE    : wait_o = req_wr || req_rd;
+            IDLE    : wait_o = crreq_i || cwreq_i;
             AR_CH   : wait_o = 1'b1;
             //R_CH    : wait_o = ~rdfin;
             R_CH    : wait_o = ~rhns;
             AW_CH   : wait_o = 1'b1;
             W_CH    : wait_o = ~wrfin;
-            B_CH    : wait_o = req_wr || req_rd;
+            B_CH    : wait_o = crreq_i || cwreq_i;
             default : wait_o = 1'b0;
         endcase
     end
